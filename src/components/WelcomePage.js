09a5df5e46ad8e7f42af025304f1f285d9f53cdd
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-05 13:05:00 +0200

    Add welcome page and root css

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ImageContainer } from './WelcomePageStyling';
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <ImageContainer>
+        <secondHeader>Eco-Friendly</secondHeader>
+      </ImageContainer>
+    </div>
+  )
+};
+
+export default WelcomePage;
---
+++ src/components/WelcomePageStyling.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ImageContainer = styled.div`
+margin: 20px;
+border: 10px solid var(--border-color-one);
+width: 70%;
+`
+
+export const secondHeader = styled.h2`
+font-weight: 700;
+`
