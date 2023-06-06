c011b61e0a4114c3fed5e07dcf9916b9db88bb1d
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-06 10:53:23 +0200

    Change urls

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,4 @@
-const BASE_URL = process.env.REACT_APP_FINAL_PROJECT_API_KEY;
+// const BASE_URL = process.env.REACT_APP_FINAL_PROJECT_API_KEY;
+const BASE_URL = 'https://project-final-project-api-backend-kpnlmcrmoq-no.a.run.app'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
