0473cebf08eaee849248bec2f26a0776d08e946e
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-08 16:41:14 +0200

    Update treatments with text description

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setItems } from 'reducers/treatments';
 import { Card, CardContainer, StyledSecondHeadingCards } from './CardStyling';
@@ -9,6 +9,8 @@ import { API_URL } from '../utils/urls';
 const Cards = () => {
   const dispatch = useDispatch();
   const treatments = useSelector((state) => state.treatments.items);
+  const [selectedTreatment, setSelectedTreatment] = useState(null);
+  const [isTreatmentSelected, setIsTreatmentSelected] = useState(false);
 
   const url = API_URL('treatments');
 
@@ -30,10 +32,20 @@ const Cards = () => {
     fetchTreatments();
   }, [dispatch, url]);
 
+  const handleTreatmentClick = (treatment) => {
+    setSelectedTreatment(treatment);
+    setIsTreatmentSelected(true);
+  };
+
+  if (isTreatmentSelected && selectedTreatment) {
+    const SelectedTreatmentText = treatments.find((treatment) => treatment._id === selectedTreatment._id).text;
+    return <div>{SelectedTreatmentText}</div>; // Display the selected treatment text
+  }
+
   return (
     <CardContainer>
       {treatments.map((treatment) => (
-        <Card key={treatment._id}>
+        <Card key={treatment._id} onClick={() => handleTreatmentClick(treatment)}>
           <img src={treatment.icon} alt="Card Icon" />
           <StyledSecondHeadingCards>{treatment.cut || treatment.wash || treatment.cutAndWash || treatment.styling}</StyledSecondHeadingCards>
         </Card>
