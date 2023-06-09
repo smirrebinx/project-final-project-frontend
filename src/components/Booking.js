2a44d23afc816be9f4bce1d6cba12d7c0daa8806
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-09 14:05:11 +0200

    Add react datepicker

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,16 +1,34 @@
+// import React, { useState } from 'react';
+// import Calendar from 'react-calendar'
+// import 'react-calendar/dist/Calendar.css';
+// import { CalendarContainer } from './BookingStyling';
+
+// const Booking = () => {
+//   const [value, onChange] = useState(new Date());
+
+//   return (
+//     <CalendarContainer>
+//       <Calendar onChange={onChange} value={value} locale="en-GB" />
+//     </CalendarContainer>
+//   );
+// }
+
+// export default Booking;
+
 import React, { useState } from 'react';
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
-import { CalendarContainer } from './BookingStyling';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 const Booking = () => {
-  const [value, onChange] = useState(new Date());
-
+  const [startDate, setStartDate] = useState(new Date());
   return (
-    <CalendarContainer>
-      <Calendar onChange={onChange} value={value} locale="en-GB" />
-    </CalendarContainer>
+    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    // <DatePicker selected={date} onChange={handleDateChange} showTimeSelect dateFormat="Pp" />
   );
-}
+};
 
 export default Booking;
