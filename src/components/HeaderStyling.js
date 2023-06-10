066a0f12f40962272122d1292db5b0a623f75901
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-10 11:44:40 +0200

    Update letter-spacing

--- src/components/HeaderStyling.js
+++ src/components/HeaderStyling.js
@@ -23,6 +23,7 @@ export const StyledHeader = styled.h1`
 font-family: var(--headings-font-family-one);
 font-weight: 600;
 font-size: 4.875rem;
+letter-spacing: 0.05em;
 text-transform: uppercase;
 margin: 1.25rem;
 color: var(--circle-border-color-one);
@@ -30,6 +31,7 @@ color: var(--circle-border-color-one);
 export const StyledSecondHeader = styled.h2`
 font-weight: 400;
 font-size: 1.5rem;
+letter-spacing: 0.05em;
 font-family:var(--paragraph-font-family-one);
 text-transform: uppercase;
 margin: 1.5rem;
--- src/components/WelcomePageStyling.js
+++ src/components/WelcomePageStyling.js
@@ -1,11 +1,13 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const ImageContainer = styled.div`
-margin: 20px;
-border: 10px solid var(--border-color-one);
-width: 70%;
-`
+// export const ImageContainer = styled.div`
+// margin: 20px;
+// border: 10px solid var(--border-color-one);
+// width: 70%;
+// `
 
-export const SecondHeader = styled.h2`
-font-weight: 700;
-`
+// export const SecondHeader = styled.h2`
+// font-weight: 700;
+// font-family: var(--headings-font-family-one);
+// letter-spacing: 0.05em;
+// `
