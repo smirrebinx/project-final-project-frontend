7b64b7a22d72223bbd260220d88dec7688374251
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-11 08:25:13 +0200

    Add sticky navbar

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import classNames from 'classnames';
 import { usePickedDate } from './Booking';
 import Footer from './Footer';
+import useSticky from './useSticky';
 import { OuterWrapper } from './GlobalStyling';
+import { StickyNavTwo, StyledNavHeaderTwo } from './NavbarStyling';
 
 const UserInfo = () => {
+  const { sticky, stickyRef } = useSticky();
   const pickedDate = usePickedDate();
 
   // Use the pickedDate value in the component
 
   return (
-    <OuterWrapper>
-      <div>
-        <h1>User Information</h1>
-        <p>Picked Date: {pickedDate}</p>
-      </div><Footer />
-    </OuterWrapper>
+    <>
+      <StickyNavTwo ref={stickyRef} className={classNames({ sticky })}>
+        <StyledNavHeaderTwo>Log In or Register</StyledNavHeaderTwo>
+      </StickyNavTwo>
+      <OuterWrapper>
+        <div>
+          <h1>User Information</h1>
+          <p>Picked Date: {pickedDate}</p>
+        </div><Footer />
+      </OuterWrapper>
+    </>
   );
 };
 
