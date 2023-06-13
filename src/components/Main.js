b9aa2a81d7a9fbc44dcfb1a29ab1d59966a6b320
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-13 15:22:14 +0200

    Remove log out button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { useNavigate, Routes, Route } from 'react-router-dom';
-import user from 'reducers/user';
-import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import Login from './Login';
 import Booking from './Booking';
@@ -9,37 +7,16 @@ import Cards from './Cards';
 import UserInfo from './UserInfo';
 
 const Main = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleLogout = () => {
-    dispatch(user.actions.logout());
-    dispatch(user.actions.setEmail(null));
-    dispatch(user.actions.setUserId(null));
-    dispatch(user.actions.setAccessToken(null));
-    localStorage.removeItem('user');
-    navigate('/login');
-    console.log('logged out');
-  };
-
   return (
-    <section>
-      <div className="form-container">
-        <div className="button-container">
-          <button className="logout-button" type="button" onClick={handleLogout}>
-            Log out
-          </button>
-        </div>
-      </div>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/calendar" element={<Booking />} />
-        <Route path="/cards" element={<Cards />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/userinfo" element={<UserInfo />} />
-      </Routes>
-    </section>
+
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/calendar" element={<Booking />} />
+      <Route path="/cards" element={<Cards />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/userinfo" element={<UserInfo />} />
+    </Routes>
   );
 };
 
