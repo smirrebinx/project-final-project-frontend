c7f0ee97e8cf8f3a52057c9e00f1c591c723f6e3
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-15 14:54:56 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <FooterContainer>
       <StyledFooterHeaderTwo>Created by Michelle Wegler.</StyledFooterHeaderTwo>
-      <StyledFooterHeaderTwo>User by ainul muttaqin from <a href="https://thenounproject.com/browse/icons/term/user/" target="_blank" title="User Icons" rel="noreferrer noopener">Noun Project</a></StyledFooterHeaderTwo>
     </FooterContainer>
   )
 }
