eb78cb44e9dc710fa72af1055bf8bd9cd5fba549
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-06-25 17:21:10 +0200

    Change width lottie player

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,20 +5,24 @@ import { InnerWrapper, StyledLink } from './GlobalStyling';
 const NotFound = ({ loader }) => {
   if (loader) {
     return (
-      <>
+      <InnerWrapper>
+        <h1>Page Not Found</h1>
         <Player
           src="https://assets6.lottiefiles.com/private_files/lf30_vjigkpcg.json"
           className="lottie"
           loop
           autoplay
           speed={1}
-          style={{ height: '30rem', width: '30rem' }}
+          style={{ height: '20rem', width: '60%' }}
           aria-label="404 Not Found" />
-        <InnerWrapper><StyledLink to="/">Go to the home page</StyledLink></InnerWrapper>
-      </>
+        <InnerWrapper>
+          <StyledLink to="/">Go to the home page</StyledLink>
+        </InnerWrapper>
+      </InnerWrapper>
     );
   }
-}
+
+  return null;
+};
 
 export default NotFound;
-
